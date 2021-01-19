73497c599fe3cca3e1a600bc6c62469660f8047d
Artūrs Alsiņš <62400484+all-sins@users.noreply.github.com>
2021-01-19 19:54:39 +0200

    Added maven project structure, initial db migration script for flyway, added spring-boot dependencies to pom.xml.

---
+++ src/main/java/jnetgraph/JNetGraph.java
@@ -0,0 +1,19 @@
+package jnetgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JNetGraph {
+
+    public static void main(String[] args) {
+
+        // Run the Spring application.
+        SpringApplication.run(JNetGraph.class, args);
+
+        // Placeholder code.
+        System.out.println("Our super cool application!");
+
+    }
+
+}
